cdf43733220c0b8845ae01c4d93a66b5c430f814
zabihi <mz2019@gmail.com>
2018-07-26 17:59:46 +0430

    Session02: add Discount example

---
+++ Session02/Discount/Discount/ICalculateDiscount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount
+{
+    public interface ICalculateDiscount
+    {
+        int CalculateDiscount();
+    }
+
+    public class CalcluateDiscountGold : ICalculateDiscount
+    {
+        public int CalculateDiscount()
+        {
+            return 1;
+        }
+    }
+
+    public class CalculateDiscountSilver : ICalculateDiscount
+    {
+        public int CalculateDiscount()
+        {
+            return 2;
+        }
+    }
+
+    public class CalcuateDiscountBoronz : ICalculateDiscount
+    {
+        public int CalculateDiscount()
+        {
+            return 3;
+        }
+    }
+}
